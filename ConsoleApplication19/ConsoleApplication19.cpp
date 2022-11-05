0f14f4f5b895e014a3b8c283da1072f0ede39f06
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-11-05 20:56:46 +0500

    Update ConsoleApplication19.cpp

--- ConsoleApplication19/ConsoleApplication19.cpp
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -1,20 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cstdlib>
 #include <locale.h>
-
+#pragma warning(disable: 4996)
 using namespace std;
-
+ 
 struct Elem {
     int key;
     Elem* pnext;
+    static unsigned siz;
 };
-
-int first_elem(Elem** pbeg, Elem** pend, int d) {
+ 
+unsigned Elem::siz = 0;
+ 
+int first_elem(Elem* *pbeg, Elem* *pend, int d) {
     Elem* ph = new Elem;
     ph->key = d;
     ph->pnext = 0;
     *pbeg = ph;
     *pend = ph;
-
+    ++Elem::siz;
+ 
     return 0;
 }
 int add_elem(Elem** pend, int d) {
@@ -23,42 +28,82 @@ int add_elem(Elem** pend, int d) {
     ph->pnext = 0;
     (*pend)->pnext = ph;
     *pend = ph;
+    ++Elem::siz;
 
     return 0;
 }
-int del_elem(Elem** pbeg, Elem** pend, int k) {
-    Elem* pk = 0;
-    Elem* ph = *pbeg;
-    if (!*pbeg)
-        cout << "Ошибка: список пуст" << endl;
-    return 0;
+    int del_elem (Elem * pbeg, int k) {
+        int n;
+        cout << "Введите ключ нового элемента списка: "; cin >> n;
 
+        while (pbeg) {
+            if (pbeg->key == k) {
+                Elem* ph = new Elem;
+
+                ph->pnext = pbeg->pnext;
+                ph->key = pbeg->key;
+
+                pbeg->pnext = ph;
+                pbeg->key = n;
+
+                break;
+            }
+
+            pbeg = pbeg->pnext;
+        }
+
+        return 1;
+    }
+
+int del_elem(Elem* *pbeg, Elem* *pend, int k) {
+    Elem* tmp = *pbeg;
+    if (!*pbeg) {
+        cout << "Ошибка: список пуст" << endl;
+        return 0;
+    }
+ 
     if ((*pbeg)->key == k) {
-        if (*pbeg = *pend) {
+        if (*pbeg == *pend) {
             *pbeg = 0;
             *pend = 0;
-            delete pk;
+            delete tmp;
+            --Elem::siz;
         }
         else {
             *pbeg = (*pbeg)->pnext;
-            delete ph;
+            delete tmp;
+            --Elem::siz;
         }
     }
     else {
-        while ((ph->pnext) && (ph->pnext->key != k))
-            ph = ph->pnext;
-        pk = ph->pnext;
-        if (pk) {
-            ph->pnext = pk->pnext;
-            delete pk;
+        while (tmp->pnext && tmp->pnext->key != k) {
+            tmp = tmp->pnext;
         }
-        else
+        if (tmp->pnext) {
+            Elem* tmp1 = tmp->pnext->pnext;
+            delete tmp->pnext;
+            tmp->pnext = tmp1;
+            --Elem::siz;
+            if (!tmp->pnext) {
+                *pend = tmp;
+            }
+        }
+        else{
             return 0;
+        }
     }
     return 1;
 }
-void work(Elem** pbeg, int z) {}
-void print_elem(Elem* pbeg) {
+void del_list(Elem *pbeg) {
+    if (pbeg != NULL) {
+        del_list(pbeg->pnext);
+        delete pbeg;
+        --Elem::siz;
+ 
+    }
+}
+
+void print_elem(Elem *pbeg) {
     Elem* ph = pbeg;
     while (ph) {
         cout << ph->key << ' ';
@@ -66,25 +111,20 @@ void print_elem(Elem* pbeg) {
     }
     cout << endl;
 }
-void del_list(Elem* pbeg) {
-    if (pbeg != NULL) {
-        del_list(pbeg->pnext);
-        delete pbeg;
-    }
-}
 int main() {
     setlocale(LC_ALL, "Russian");
     Elem* pbeg = 0;
     Elem* pend = 0;
     int d;
-    int g = 0;
     int a;
     int k;
+    int g = 0;
     int z;
-    char s;
     char ch;
+    bool exit = false;
     do {
-        cout << " Нажмите 1 - добавить элемент/создать, 2 - удалить, 3 - просмотр списка: " << endl;
+        system("cls");
+        cout << "1 - добавить элемент/создать\n2 - удалить один\n3 - просмотр\n4 - выход" << endl;
         cin >> ch;
         switch (ch) {
         case '1':
@@ -92,49 +132,57 @@ int main() {
                 cout << "Введите 1 элемент списка: " << endl;
                 cin >> d;
                 first_elem(&pbeg, &pend, d);
+                g++;
+                print_elem(pbeg);
                 break;
             }
             else {
                 cout << "Введите новый элемент списка: " << endl;
                 cin >> a;
                 add_elem(&pend, a);
-                cout << "Список всех элементов" << endl;
                 print_elem(pbeg);
-                g = 1;
                 break;
             }
         case '2':
             cout << "Введите значение элемента для удаления: " << endl;
             cin >> k;
-            if (del_elem(&pbeg, &pend, k) == 0) {
+            if (del_elem (pbeg, k) == 0) {
                 cout << "Элемент для удаления не найден" << endl;
-                cout << "Список всех элементов" << endl;
-                print_elem(pbeg);
             }
             else {
-                del_elem(&pbeg, &pend, k);
-                cout << "Список всех элементов" << endl;
-                print_elem(pbeg);
-            }
+             
+                        if (del_elem(&pbeg, &pend, k) == 0) {
+                            cout << "Элемент для удаления не найден" << endl;
+                        }
+                        if (g > 0) {
+                            cout << "Список всех элементов" << endl;
+                            print_elem(pbeg);
+                    }
+                    else
+                        cout << "Список пуст" << endl;
+                    break;
+                 }
             break;
+
         case '3':
-            if (g == 1)
-            {
+            if (g> 0) {
                 cout << "Список всех элементов" << endl;
                 print_elem(pbeg);
-                break;
             }
             else
                 cout << "Список пуст" << endl;
+            break;
+        case '4':
+            exit = true;
+            break;
         }
         _flushall;
-        cout << "Повторить (1-да/2-нет)" << endl;
-        cin >> s;
-    } while (s == '1');
-    system("Pause");
+        system("Pause");
+    } while (!exit);
     if (pbeg != NULL) {
         del_list(pbeg->pnext);
         delete pbeg;
+        Elem::siz = 0;
     }
     return 0;
 }
